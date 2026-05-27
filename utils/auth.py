dfdf0df6854d88abc01f9730f121b94cfa686df7
alhazmia300-ux <alhazmia300@gmail.com>
2026-05-28 01:37:46 +0600

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,65 @@
- 
+ import requests
+
+# ======================================================
+# SIGN UP
+# ======================================================
+
+def signup(email, password, api_key):
+
+    url = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={api_key}"
+
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
+
+    try:
+
+        r = requests.post(
+            url,
+            json=payload,
+            timeout=10
+        )
+
+        return r.json()
+
+    except Exception as e:
+
+        return {
+            "error":{
+                "message":str(e)
+            }
+        }
+
+# ======================================================
+# LOGIN
+# ======================================================
+
+def login(email, password, api_key):
+
+    url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
+
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
+
+    try:
+
+        r = requests.post(
+            url,
+            json=payload,
+            timeout=10
+        )
+
+        return r.json()
+
+    except Exception as e:
+
+        return {
+            "error":{
+                "message":str(e)
+            }
+        }
